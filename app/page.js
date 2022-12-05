da1073707be74f161e85e731f91383fa91b2ab8b
Maksim Shelest <shelest.ks@gmail.com>
2022-12-05 15:43:33 +0300

    insights done

--- app/page.js
+++ app/page.js
@@ -7,18 +7,18 @@ const Page = () => (
     <Hero />
     <div className="relative">
       <About />
-      <div className="graditent-03 z-0" />
+      <div className="gradient-03 z-0" />
       <Explore />
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="graditent-04 z-0" />
+      <div className="gradient-04 z-0" />
       <WhatsNew />
     </div>
     <World />
     <div className="relative">
       <Insights />
-      <div className="graditent-04 z-0" />
+      <div className="gradient-04 z-0" />
       <Feedback />
     </div>
 
