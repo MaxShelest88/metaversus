e414e57d52f8ebb3463d8ea9c45cec31d07f1572
Maksim Shelest <shelest.ks@gmail.com>
2022-12-05 10:34:47 +0300

    whats new done

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion';
 import styles from '../styles';
 import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
-import { ExploreCard, StartSteps, TitleText, TypingText } from '../components';
+import { StartSteps, TitleText, TypingText } from '../components';
 import { startingFeatures } from '../constants';
 
 const GetStarted = () => (
